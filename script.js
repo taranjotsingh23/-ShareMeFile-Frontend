b439de1ac264972453da6f5d65990202c6c6e197
Unicorn GTBIT <unicorngtbit@gmail.com>
2023-01-28 02:18:01 +0530

    Added initial Animation Part

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const gotop = document.querySelector(".gotopbtn");
 
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px"
+}
+
+
 window.addEventListener("scroll", () => {
 
     if(window.pageYOffset > 100) {
